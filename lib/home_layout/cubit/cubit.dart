7735f599b831b02a9048f076f5d247733817f134
Eslam <islamch.20102@gmail.com>
2022-02-12 01:15:13 +0200

    Add files via upload

--- lib/home_layout/cubit/cubit.dart
+++ lib/home_layout/cubit/cubit.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeCubit extends Cubit<HomeState> {
+
   HomeCubit() : super(HomeInitialState());
 
 
